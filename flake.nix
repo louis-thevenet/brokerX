ff05bf63097ea81fda03f017d0ce1135414372fb
Louis Thevenet <louis.tvnt@gmail.com>
2025-09-04 15:32:10 -0400

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+
+            packages = with pkgs; [
+              # Nix
+              nil
+              alejandra
+
+              # Typst
+              typst
+              tinymist
+              typstyle
+            ];
+          };
+        };
+    };
+}
